954eb6485f8b83bcf60b2cd9b61421e019552495
Charlie Cheater <charliecheaters@yandex.ru>
2023-03-16 18:13:02 +0300

    Rename project. Removed extra classes

---
+++ Payeer.Trade.Api.Client/Utils/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Payeer.Trade.Api.Models.Enums;
+
+namespace Payeer.Trade.Api.Client.Utils
+{
+    public static class Utilities
+    {
+        public static HttpMethod GetHttpMethod(ApiMethod method)
+        {
+            return method switch
+            {
+                ApiMethod.Delete => HttpMethod.Delete,
+                ApiMethod.Get => HttpMethod.Get,
+                ApiMethod.Post => HttpMethod.Post,
+                ApiMethod.Put => HttpMethod.Put,
+                _ => throw new NotImplementedException(),
+            };
+        }
+    }
+}
