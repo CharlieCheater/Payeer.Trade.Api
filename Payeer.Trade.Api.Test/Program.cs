949b8fc30cf630b09a42af8279a423012fa6926c
Charlie Cheater <charliecheaters@yandex.ru>
2023-03-18 09:20:36 +0300

    Added console project for API testing

---
+++ Payeer.Trade.Api.Test/Program.cs
@@ -0,0 +1,10 @@
+﻿using Payeer.Trade.Api.Client;
+
+var apiId = "2aa21d00f-ba85-4ca8-b378-9d6d9a58c625d";
+var apiSecret = "1vZtJAsNyBv18c8F";
+PayeerClient client = new PayeerClient(new ApiClient(apiId, apiSecret));
+var time = await client.GetServerTimeAsync();
+if (true)
+{
+
+}
