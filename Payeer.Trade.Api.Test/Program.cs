b86bb2fff115e189b20f14feff3e2193f64205a0
Charlie Cheater <charliecheaters@yandex.ru>
2023-03-21 08:24:42 +0300

    Update

--- Payeer.Trade.Api.Test/Program.cs
+++ Payeer.Trade.Api.Test/Program.cs
@@ -1,10 +1,20 @@
 ﻿using Payeer.Trade.Api.Client;
 
-var apiId = "2aa21d00f-ba85-4ca8-b378-9d6d9a58c625d";
-var apiSecret = "1vZtJAsNyBv18c8F";
+var apiId = "Your-Api-Id";
+var apiSecret = "Your-Api-Secret";
 PayeerClient client = new PayeerClient(new ApiClient(apiId, apiSecret));
-var time = await client.GetServerTimeAsync();
-if (true)
-{
 
+var timeResult = await client.GetServerTimeAsync();
+Console.WriteLine($"Server time - {timeResult.Time}");
+
+var balanceResult = await client.GetBalanceAsync();
+
+foreach (var balance in balanceResult.Balances)
+{
+    var info = balance.Value;
+    Console.WriteLine($"{balance.Key}:" +
+                      $"\n\tTotal: {info.Total}" +
+                      $"\n\tAvailable: {info.Available}" +
+                      $"\n\tHold: {info.Hold}" +
+                      $"\n");
 }
