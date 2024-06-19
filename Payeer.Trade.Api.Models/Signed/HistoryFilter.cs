a31a393456bdc0088ea77f0a9b17a26a0dd11dbe
Charlie Cheater <charliecheaters@yandex.ru>
2024-06-19 06:17:06 +0300

    Changed the limit of the maximum gap in days

--- Payeer.Trade.Api.Models/Signed/HistoryFilter.cs
+++ Payeer.Trade.Api.Models/Signed/HistoryFilter.cs
@@ -4,10 +4,19 @@ namespace Payeer.Trade.Api.Models.Signed;
 
 public class HistoryFilter
 {
+    public const int MaxDaysDistance = 32;
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="from">Date of the beginning of the filtering period</param>
+    /// <param name="to">Date of the end of the filtering period 
+    /// the filtering period should not exceed 32 days</param>
+    /// <exception cref="ArgumentException">the filtering period should not exceed max days distance</exception>
     public HistoryFilter(DateTime from, DateTime to)
     {
-        if (from.Year == 1 && to.Year == 1)
-            throw new ArgumentException("The minimum year allowed is 2");
+        var distance = from - to;
+        if (distance.TotalDays > MaxDaysDistance)
+            throw new ArgumentException($"the filtering period should not exceed {MaxDaysDistance} days");
 
         From = from;
         To = to;
