85923937b679b5675432d255c8a7fdd03d0d21b8
Charlie Cheater <charliecheaters@yandex.ru>
2024-06-19 06:17:38 +0300

    Added the necessary summaries

--- Payeer.Trade.Api.Domain/Interfaces/IPayeerClient.cs
+++ Payeer.Trade.Api.Domain/Interfaces/IPayeerClient.cs
@@ -88,14 +88,66 @@ public interface IPayeerClient
     /// <param name="stopPrice">Stop price</param>
     /// <returns></returns>
     Task<OrderCreateResult> CreateStopLimitOrderAsync(string pair, ActionTypes action, double amount, double price, double stopPrice);
+    /// <summary>
+    /// Getting detailed information about your order by id
+    /// </summary>
+    /// <param name="orderId">order id</param>
+    /// <returns></returns>
     Task<OrderStatusResult> GetOrderStatusAsync(int orderId);
+    /// <summary>
+    /// Cancellation of your order by id
+    /// </summary>
+    /// <param name="id">order id</param>
+    /// <returns></returns>
     Task<CancelOrderResult> CancelOrderAsync(int id);
+    /// <summary>
+    /// Cancellation with pairs and directions 
+    /// </summary>
+    /// <param name="pairs">list of pairs to cancel orders</param>
+    /// <param name="action">action</param>
+    /// <returns></returns>
     Task<CancelOrderResult> CancelOrdersAsync(string[] pairs, ActionTypes? action = null);
+    /// <summary>
+    /// Cancel all orders 
+    /// </summary>
+    /// <returns></returns>
     Task<CancelOrderResult> CancelAllOrdersAsync();
+    /// <summary>
+    /// Getting your open orders
+    /// </summary>
+    /// <returns></returns>
     Task<OpenOrdersResult> GetOpenOrdersAsync();
+    /// <summary>
+    /// Getting your open orders with the ability to filtering
+    /// </summary>
+    /// <param name="pairs">list of pairs</param>
+    /// <param name="action">action</param>
+    /// <returns></returns>
     Task<OpenOrdersResult> GetOpenOrdersAsync(string[] pairs, ActionTypes action);
+    /// <summary>
+    /// Getting the history of your orders
+    /// </summary>
+    /// <returns></returns>
     Task<HistoryResult<OrderInfo>> GetAllHistoryOrdersAsync();
+    /// <summary>
+    /// Getting the history of your orders with the possibility of filtering and page-by-page loading
+    /// </summary>
+    /// <param name="filters">Filters</param>
+    /// <param name="appendOrder">id of the last known order for page-by-page loading of elements</param>
+    /// <param name="limit">the count of returned items (by default: 50)</param>
+    /// <returns></returns>
     Task<HistoryResult<OrderInfo>> GetPagedHistoryOrdersAsync(HistoryFilter? filters = null, int? appendOrder = null, int limit = 50);
+    /// <summary>
+    /// Getting your trades
+    /// </summary>
+    /// <returns></returns>
     Task<HistoryResult<PersonTradeInfo>> GetAllHistoryTradesAsync();
+    /// <summary>
+    /// Getting your trades with the possibility of filtering and page-by-page loading
+    /// </summary>
+    /// <param name="filters"></param>
+    /// <param name="appendOrder"></param>
+    /// <param name="limit"></param>
+    /// <returns></returns>
     Task<HistoryResult<PersonTradeInfo>> GetPagedHistoryTradesAsync(HistoryFilter? filters = null, int? appendOrder = null, int limit = 50);
 }
